f8009584814aaa3d2aaab8a25bbc5f8e424299be
eduard2382 <motadossantosjoseeduardo.m82@gmial.com>
2025-05-01 19:38:58 -0300

    add bar working and recording to local base

--- script.js
+++ script.js
@@ -1,12 +1,149 @@
-var btnChecks = document.querySelectorAll(".btnCheck")
+const newToDo = document.querySelector('#newToDo')
+const btnAdd = document.querySelector('#btnAdd')
 
+var toDoList = []
 
-// Percorre os checkbox, quando clicados mudam suas classes
-btnChecks.forEach((element)=>{
-    element.addEventListener('click', ()=>{
-        element.classList.toggle('ri-checkbox-fill')
-        element.classList.toggle('ri-checkbox-blank-line')
-    })
+btnAdd.addEventListener('click', ()=>{
+    //Adiciona o evento de 'click' no botao de adicionar e chama a funcao de nova tarefa passando o texto do input como parametro
+
+    let toDoName = newToDo.value
+
+    if(toDoName.length > 0){
+        addNewToDo(toDoName)
+    }
 })
 
+checkLocalStorage()
+
+function checkLocalStorage(){
+    if(localStorage.getItem('toDoList') != null){
+        toDoList = JSON.parse(localStorage.getItem('toDoList'))
+
+        renderToDoList()
+    }
+}
+
+function updateLocalStorage(){
+    localStorage.setItem('toDoList', JSON.stringify(toDoList))
+}
+
+function renderToDoList(){
+    document.querySelector('main').innerHTML = ''
+
+    toDoList.forEach((toDo)=>{
+        renderToDo(toDo.name, toDo.selected)
+    })
+}
+
+function addNewToDo(toDoName){
+    //Adiciona a tarefa no array de tarefas e chama a função de renderizar na tela
+
+    addToDoList(toDoName)
+
+    renderToDo(toDoName)
+}
+
+function addToDoList(toDoName){
+    toDoList.push({
+        name: toDoName,
+        selected: false
+    })
+
+    localStorage.setItem('toDoList', JSON.stringify(toDoList))
+}
+
+function renderToDo(toDoName, state=false){
+    //Renderiza a tarefa na tela
+
+    console.log(state)
+
+    if(toDoList.length == 1){
+        document.querySelector('main').innerHTML = ''
+        createElementToDo(toDoName, state)
+    } else{
+        createElementToDo(toDoName, state)
+    }
+}
+
+function createElementToDo(toDoName, state){
+    //Cria os containers necessarios e adicionar os outros elementos neles, depois os adiciona no 'main'
+
+    let toDoContainer = document.createElement('div')
+    toDoContainer.className = 'toDoContainer'
+    let toDo = document.createElement('span')
+    toDo.className = 'toDo'
+    toDo.appendChild(createCheckbox(state))
+    toDo.appendChild(createName(toDoName))
+
+    toDoContainer.appendChild(toDo)
+    toDoContainer.appendChild(createTrash())
+
+    document.querySelector('main').appendChild(toDoContainer)
+}
+
+function createName(toDoName){
+    //Cria e retorna o elemento 'p' que contem o nome da tarefa
+
+    let name = document.createElement('p')
+    name.classList.add('toDoName')
+    name.innerText = toDoName
+
+    return name
+}
+
+function createCheckbox(state=false){
+    //Cria e retorna o icone de checkbox
+
+    let checkbox = document.createElement('i')
+    
+    if(state == false){
+        checkbox.className = 'ri-checkbox-blank-line btnCheck'
+    } else{
+        checkbox.className = 'ri-checkbox-fill btnCheck'
+    }
+
+
+    addClickToCheckbox(checkbox)
+    
+    return checkbox
+}
+
+function createTrash(){
+    //Cria e retorna o icone de lixeira
+
+    let trash = document.createElement('i')
+    trash.className = 'ri-delete-bin-fill btnTash'
+
+    return trash
+}
+
+function addClickToCheckbox(checkbox){
+    //Adiciona o evento de 'click' no checkbox
+
+    checkbox.addEventListener('click', ()=>{
+        toggleStateCheckbox(checkbox)
+        toggleStateToDo(checkbox)
+    })
+}
+
+function toggleStateCheckbox(checkbox){
+    checkbox.classList.toggle('ri-checkbox-fill')
+    checkbox.classList.toggle('ri-checkbox-blank-line')
+}
+
+function toggleStateToDo(checkbox){
+    let checkboxList = document.querySelectorAll('.btnCheck')
+
+    checkboxList.forEach((element, index)=>{
+        if(checkbox == element){
+            if(toDoList[index].selected == false){
+                toDoList[index].selected = true
+            } else{
+                toDoList[index].selected = false
+            }
+        }
+    })
+
+    updateLocalStorage()
+}
 
