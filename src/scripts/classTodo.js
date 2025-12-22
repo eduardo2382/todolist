3fb5527131cb5e36a8cd88c60d2d7559711780d1
dudu2382 <motadossantosjoseeduardo.m82@gmail.com>
2025-12-22 19:48:41 -0300

    adicionando os todos no storage

--- src/scripts/classTodo.js
+++ src/scripts/classTodo.js
@@ -1,6 +1,34 @@
-class Todo{
-    constructor(nameAtr){
-        this.name = nameAtr
+import { createElement } from "react";
+
+export class Todo{
+    #elementTodo;
+
+    constructor(name){
+        this.name = name
+    }
+
+    #createElementTodo(){
+        this.#elementTodo = createElement('div')
+
+        let todoSpan = createElement('span')
+        todoSpan.setAttribute('class', 'todo')
+
+        let inputRadius = createElement('i')
+        inputRadius.classList.add('ri-checkbox-blank-circle-line') 
+        inputRadius.classLisit.add('btnCheckTodo')
+        todoSpan.appendChild(inputRadius)
+
+        let nameTodo = createElement('p')
+        nameTodo.innerText = this.name
+        todoSpan.appendChild(nameTodo)
+
+        this.#elementTodo.appendChild(todoSpan)
+
+        let btnDelete = createElement('i')
+        btnDelete.classList.add('ri-delete-bin-7-line')
+        btnDelete.classList.add('btnDeleteTodo')
+
+        this.#elementTodo.appendChild(btnDelete)
     }
 
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,21 @@
 import { Storage } from './storage.js'
+import { Todo } from './classTodo.js'
 
 const inputNewTodo = document.querySelector('#inputNewTodo')
 const btnAddTodo = document.querySelector('#btnAddTodo')
 
 const storage = new Storage("todolist")
 
+
+btnAddTodo.addEventListener('click', ()=>{
+    let todoName = inputNewTodo.value
+    if(todoName.length > 0){
+        let todo = new Todo(todoName)
+
+        storage.addStorage(todo)
+
+        inputNewTodo.value = ""
+        inputNewTodo.focus()
+    }
+});
 
